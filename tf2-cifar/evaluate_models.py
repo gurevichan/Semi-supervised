9888e2bf182ae9efe032d9aef437704ee3029849
Andrey Gurevich <gurevichan@gmail.com>
2022-08-04 09:21:26 +0300

    updating fixed evaluate models

--- tf2-cifar/evaluate_models.py
+++ tf2-cifar/evaluate_models.py
@@ -3,12 +3,15 @@ import utils
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='TensorFlow2.0 CIFAR-10 Training')
+    parser.add_argument('checkpoint_paths', nargs='+', type=str, help="path to the model's checkpoint")
     parser.add_argument('--model', default='resnet18', type=str, help='model type')
-    parser.add_argument('--checkpoint_path', required=True, type=str, help="path to the model's checkpoint")
     args = parser.parse_args()
     args.model = args.model.lower()
-    
+    train_ds, test_ds, decay_steps = utils.prepare_data(1.0, 256, 1)
+
     print('==> Building model...')
     model = utils.create_model(args.model, num_classes=10)
-    utils.load_weights_to_model(model, args.checkpoint_path)
-    utils.evaluate_model(model)
+    for checkpoint_path in args.checkpoint_paths:
+        print(checkpoint_path)
+        utils.load_weights_to_model(model, checkpoint_path)
+        utils.evaluate_model(model, test_ds=test_ds)
