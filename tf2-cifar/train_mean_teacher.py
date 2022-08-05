b91c4e79c1c592d28e5d568366db8e18a6113d1c
Andrey Gurevich <andrey.gurevich@mobileye.com>
2022-08-05 06:30:35 +0000

    adding progressinve ema_weight and consistency weight

--- tf2-cifar/train_mean_teacher.py
+++ tf2-cifar/train_mean_teacher.py
@@ -16,7 +16,8 @@ import utils
 
 class MeanTeacher(SupervisedTrainer):
 
-    def __init__(self, model_type, decay_steps, lr, num_classes=10, train_data_fraction=1.0, resume=False, wandb=None):
+    def __init__(self, model_type, decay_steps, lr, num_classes=10, train_data_fraction=1.0, resume=False, wandb=None,
+                 consistency_weight=1.):
         super(MeanTeacher, self).__init__(model_type, decay_steps, lr, num_classes=num_classes, train_data_fraction=train_data_fraction, resume=resume, wandb=wandb)
         self.checkpoint_path = f'./checkpoints/{args.model}/MeanTeacher/train_frac_{args.train_data_fraction}'
         self.teacher = copy.deepcopy(self.model)
@@ -24,10 +25,11 @@ class MeanTeacher(SupervisedTrainer):
         self.l2_loss = tf.keras.losses.MeanSquaredError()
         self.consistency_loss_metric = tf.keras.metrics.Mean(name='consistency_loss_metric')
         self.test_accuracy_teacher = tf.keras.metrics.CategoricalAccuracy(name='test_accuracy_teacher')
+        self.consistency_weight = consistency_weight
 
-    def ema_teacher_weights(self):
+    def ema_teacher_weights(self, ema_alpha):
         for teacher_weight, model_weight in zip(self.teacher.trainable_variables, self.model.trainable_variables):
-            teacher_weight.assign(self.ema_alpha * teacher_weight + (1 - self.ema_alpha) * model_weight)
+            teacher_weight.assign(ema_alpha * teacher_weight + (1 - ema_alpha) * model_weight)
 
     def my_teacher_weights_ema(self):
         teacher_weights = self.teacher.get_weights()
@@ -40,6 +42,7 @@ class MeanTeacher(SupervisedTrainer):
         best_acc, curr_epoch, manager = self._init_train_step()
 
         for e in tqdm(range(int(curr_epoch), epoch)):
+            training_progress = e/float(epoch)
             self.train_loss.reset_states()
             self.train_accuracy.reset_states()
             self.test_loss.reset_states()
@@ -47,7 +50,7 @@ class MeanTeacher(SupervisedTrainer):
             self.consistency_loss_metric.reset_states()
 
             for (images, labels), unlabeled_imgs in zip(train_ds, unlabeled_ds):
-                self.train_step(images, labels, unlabeled_imgs)
+                self.train_step(images, labels, unlabeled_imgs, training_progress=training_progress)
 
             for images, labels in test_ds:
                 self.test_step(images, labels)
@@ -56,11 +59,11 @@ class MeanTeacher(SupervisedTrainer):
             self.save_checkpoint(best_acc, curr_epoch, manager, epoch=e)
 
     @tf.function  #(jit_compile=True)
-    def train_step(self, images, labels, unlabeled_imgs):
+    def train_step(self, images, labels, unlabeled_imgs, training_progress):
         u1, _ = utils._augment_fn(unlabeled_imgs, {})
         u2, _ = utils._augment_fn(unlabeled_imgs, {})
         teacher_pred = self.teacher(u1, training=True)
-
+        consistency_weight, ema_alpha = self._get_weight_decay_and_ema_alpha(training_progress)
         with tf.GradientTape() as tape:
             labeled_preds = self.model(images, training=True)
             unlabeled_preds = self.model(u2, training=True)
@@ -69,21 +72,33 @@ class MeanTeacher(SupervisedTrainer):
             consistency_loss = self.l2_loss(unlabeled_preds, teacher_pred)
             weight_decay_loss = tf.add_n([tf.nn.l2_loss(v) for v in self.model.trainable_variables])
 
-            loss = supervised_loss + weight_decay_loss * self.weight_decay + consistency_loss
+            loss = supervised_loss + weight_decay_loss * self.weight_decay + consistency_loss * consistency_weight
 
         gradients = tape.gradient(loss, self.model.trainable_variables)
         self.optimizer.apply_gradients(zip(gradients, self.model.trainable_variables))
         self.train_loss(loss)
         self.train_accuracy(labels, labeled_preds)
 
-        self.ema_teacher_weights()
+        self.ema_teacher_weights(ema_alpha)
         self.consistency_loss_metric(consistency_loss)
 
+    def _get_weight_decay_and_ema_alpha(self, training_progress):
+        """
+        training_progress is between (0,1)
+        """
+        start_consistency_weight = 0.0
+        end_consistency_weight = self.consistency_weight
+        start_ema_alpha = 0.97
+        end_ema_alpha = 0.99
+        consistency_weight = (1 - training_progress) * start_consistency_weight + training_progress * end_consistency_weight
+        ema_alpha = (1 - training_progress) * start_ema_alpha + training_progress * end_ema_alpha
+        print(f'{training_progress=:.2f}, {consistency_weight=:.2f}, {ema_alpha=:.4f}')
+        return consistency_weight, ema_alpha
 
     def _log(self, e):
         super()._log(e)
         print(f'Consistency loss: {self.consistency_loss_metric.result():.4f}, ' \
-              f'Test Accuracy Teacher: {self.test_accuracy_teacher.result()*100:.2f}')
+              f'Test Accuracy Teacher: {self.test_accuracy_teacher.result()*100:.2f}%')
         if self.wandb:
             self.wandb.log({'consistency_loss': self.consistency_loss_metric.result(),
                             'test_accuracy_teacher': self.test_accuracy_teacher.result()*100})
@@ -118,7 +133,7 @@ if __name__ == "__main__":
     parser.add_argument('--train_data_fraction', default=1.0, type=float, help='fraction of data to use in train')
     parser.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint')
     parser.add_argument('--gpu', default=0, type=int, help='specify which gpu to be used')
-    parser.add_argument('--consistensy_weight', '-cw', default=1, type=float, help='specify which gpu to be used')
+    parser.add_argument('--consistency_weight', '-cw', default=1, type=float, help='specify which gpu to be used')
     args = parser.parse_args()
     args.model = args.model.lower()
 
