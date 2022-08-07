ab71e72ab8a1ac44761118cc99b1175631b4a9f0
Andrey Gurevich <andrey.gurevich@mobileye.com>
2022-08-07 15:08:46 +0000

    (i think) working mean teacher

--- tf2-cifar/train_mean_teacher.py
+++ tf2-cifar/train_mean_teacher.py
@@ -19,11 +19,12 @@ mixed_precision.set_global_policy(policy)
 class MeanTeacher(SupervisedTrainer):
 
     def __init__(self, model_type, decay_steps, lr, num_classes=10, train_data_fraction=1.0, 
-                 resume=False, wandb=None, consistency_weight=1.):
+                 resume=False, wandb=None, consistency_weight=1., ema_start=0.99):
         super(MeanTeacher, self).__init__(model_type, decay_steps, lr, num_classes=num_classes, 
                                           train_data_fraction=train_data_fraction, resume=resume, wandb=wandb)
         self.checkpoint_path = f'./checkpoints/{model_type}/MeanTeacher/train_frac_{train_data_fraction}'
         self.teacher = copy.deepcopy(self.model)
+        self.ema_start = ema_start
         self.l2_loss = tf.keras.losses.MeanSquaredError()
         self.consistency_loss_metric = tf.keras.metrics.Mean(name='consistency_loss_metric')
         self.test_accuracy_teacher = tf.keras.metrics.CategoricalAccuracy(name='test_accuracy_teacher')
@@ -57,7 +58,7 @@ class MeanTeacher(SupervisedTrainer):
     def train_step(self, images, labels, unlabeled_imgs, training_progress):
         u1 = unlabeled_imgs
         u2, _ = utils._augment_fn(unlabeled_imgs, {}, adjust_colors=True)
-        u1_pred = self.teacher(u1, training=False)
+        u1_pred = self.teacher(u1, training=True)
         consistency_weight, ema_alpha = self._get_weight_decay_and_ema_alpha(training_progress)
         with tf.GradientTape() as tape:
             labeled_preds = self.model(images, training=True)
@@ -65,7 +66,7 @@ class MeanTeacher(SupervisedTrainer):
             u2_preds = self.model(u2, training=True)
 
             supervised_loss = self.categorical_cross_entropy(labels, labeled_preds)
-            consistency_loss = self.l2_loss(u1_pred, u2_preds)
+            consistency_loss = self.l2_loss(u1_pred, u2_preds, sample_weight=1)
             weight_decay_loss = tf.add_n([tf.nn.l2_loss(v) for v in self.model.trainable_variables])
 
             loss = supervised_loss + weight_decay_loss * self.weight_decay + consistency_loss * consistency_weight
@@ -82,10 +83,9 @@ class MeanTeacher(SupervisedTrainer):
         """
         start_consistency_weight = 0.0
         end_consistency_weight = self.consistency_weight
-        start_ema_alpha = 0.95
         end_ema_alpha = 0.995
         consistency_weight = (1 - training_progress) * start_consistency_weight + training_progress * end_consistency_weight
-        ema_alpha = (1 - training_progress) * start_ema_alpha + training_progress * end_ema_alpha
+        ema_alpha = (1 - training_progress) * self.ema_start + training_progress * end_ema_alpha
         if verbose:
             print(f'{training_progress=:.2f}, {consistency_weight=:.2f}, {ema_alpha=:.4f}')
         return consistency_weight, ema_alpha
@@ -102,13 +102,14 @@ class MeanTeacher(SupervisedTrainer):
     @tf.function
     def test_step(self, images, labels):
         super().test_step(images, labels)
-        # predictions = self.teacher(images, training=False)
-        # self.test_accuracy_teacher(labels, predictions)
+        predictions = self.teacher(images, training=False)
+        self.test_accuracy_teacher(labels, predictions)
 
 def main():
     parser, wandb = _init_args()
     parser.add_argument('--consistency_weight', '-cw', default=1, type=float, help='specify consistency weight')
     parser.add_argument('--unlabled_bs_multiplier', '-bsm', default=2, type=int, help='specify batch size multiplier for unlabled data, will increase unlabled data fraction')
+    parser.add_argument('--ema', default=0.99, type=float, help='ema weight to start with')
     args = parser.parse_args()
     args.model = args.model.lower()
     wandb.config.update(args)
@@ -122,7 +123,7 @@ def main():
     print('==> Building model...')
     trainer = MeanTeacher(args.model, decay_steps, lr=args.lr, num_classes=10, 
                           train_data_fraction=args.train_data_fraction, resume=args.resume, 
-                          wandb=wandb, consistency_weight=args.consistency_weight)
+                          wandb=wandb, consistency_weight=args.consistency_weight, ema_start=args.ema)
     trainer.train(train_ds, test_ds, args.epoch, unlabeled_ds=unlabeled_ds)
     # Evaluate
     utils.evaluate_model(trainer.model, test_ds)
